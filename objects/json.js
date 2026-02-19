4b8ecccdd8b4fa8c273d4910b84f88547b6a73df
Panagiotis <pbekris8@gmail.com>
2026-02-19 18:27:13 +0200

    chore: add comments

--- objects/json.js
+++ objects/json.js
@@ -4,10 +4,10 @@ const jsObj = {
    population: "3000000",
 };
 
-//convert to JSON string
+//convert to JSON string -- serialization
 const jsonString = JSON.stringify(jsObj);
 console.log(jsonString); // {"id":1,"title":"Athens","population":"3000000"}
 
-// convert back to Javascript object
+// convert back to Javascript object -- deserialization
 const jsObj2 = JSON.parse(jsonString);
 console.log(jsObj2); // { id: 1, title: 'Athens', population: '3000000' }
