fdedad141ee5102cb686a9915c74ff38afa05722
Panagiotis <pbekris8@gmail.com>
2026-02-19 19:53:48 +0200

    feat: add function logic

---
+++ functions/functions.js
@@ -0,0 +1,47 @@
+// 1. Basic function syntax
+function sayHello(name) {
+   return `Hello ${name}!`;
+}
+
+console.log(sayHello("Alice"));
+
+// 2. Function Expression
+const add = function (a, b) {
+   return a + b;
+};
+
+console.log(add(3, 5)); // 8
+
+// 3. Arrow function
+const multiply = (a, b) => a * b; // υποννοειται το return
+console.log(multiply(4, 6)); // 24
+
+// 4. Optional params
+function sayHi(name) {
+   if (!name) {
+      name = "Guest";
+   }
+   console.log(`Hi ${name}`);
+}
+
+sayHi(); // hi guest
+
+//rewatch 7:20 ωρα
+// 5. Default params, Overloading
+function sayHi2(name = "Guest") {
+   console.log(`Hi ${name}`);
+}
+
+sayHi2(); // Hi Guest
+
+function addUser(name = "Anonymous", age = 18, isAdmin = false) {
+   return { name, age, isAdmin };
+}
+
+// 6. Rest parameters
+// rewatch 7:25 απο πριν συνεχεια
+function max(...numbers) {
+   return Math.max(...numbers);
+}
+
+console.log(max(1, 2, 3, 9, 6, 5, 4, 8, 7, 0)); // 9
